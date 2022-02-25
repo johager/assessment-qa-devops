6444a6fee7691cbbbcb3d475d74a10f8c2afd806
James Hager <james@avantiapplications.com>
2022-02-25 10:47:10 -0700

    Completed automated tests (UI tests)

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -13,8 +13,40 @@ afterAll(async () => {
     driver.quit()
 })
 
+async function clickDraw() {
+    await driver.findElement(By.id('draw')).click()
+}
+
+async function clickAddToDuo() {
+    await driver.findElement(By.xpath('//*[@class="bot-btn"]')).click()
+}
+
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Div #choices shows up after select Draw', async () => {
+    await clickDraw()
+    const divChoices = await driver.findElement(By.id('choices'))
+    const displayed = await divChoices.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Div #player-duo shows up after select Add to Duo', async () => {
+    await clickDraw()
+    await clickAddToDuo()
+    const divPlayerDuo = await driver.findElement(By.id('player-duo'))
+    const displayed = await divPlayerDuo.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Button #duel shows up after select two (2) Add to Duo', async () => {
+    await clickDraw()
+    await clickAddToDuo()
+    await clickAddToDuo()
+    const divPlayerDuo = await driver.findElement(By.id('duel'))
+    const displayed = await divPlayerDuo.isDisplayed()
     expect(displayed).toBe(true)
 })
