c79f434a9e590de9f05a6cbe4b5c94990f34bff2
James Hager <james@avantiapplications.com>
2022-02-25 12:24:54 -0700

    Return all UI tests to be active

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -21,18 +21,18 @@ async function clickAddToDuo() {
     await driver.findElement(By.xpath('//*[@class="bot-btn"]')).click()
 }
 
-// test('Title shows up when page loads', async () => {
-//     const title = await driver.findElement(By.id('title'))
-//     const displayed = await title.isDisplayed()
-//     expect(displayed).toBe(true)
-// })
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
 
-// test('Div #choices shows up after select Draw', async () => {
-//     await clickDraw()
-//     const divChoices = await driver.findElement(By.id('choices'))
-//     const displayed = await divChoices.isDisplayed()
-//     expect(displayed).toBe(true)
-// })
+test('Div #choices shows up after select Draw', async () => {
+    await clickDraw()
+    const divChoices = await driver.findElement(By.id('choices'))
+    const displayed = await divChoices.isDisplayed()
+    expect(displayed).toBe(true)
+})
 
 test('Div #player-duo shows up after select Add to Duo', async () => {
     await clickDraw()
@@ -42,11 +42,11 @@ test('Div #player-duo shows up after select Add to Duo', async () => {
     expect(displayed).toBe(true)
 })
 
-// test('Button #duel shows up after select two (2) Add to Duo', async () => {
-//     await clickDraw()
-//     await clickAddToDuo()
-//     await clickAddToDuo()
-//     const divPlayerDuo = await driver.findElement(By.id('duel'))
-//     const displayed = await divPlayerDuo.isDisplayed()
-//     expect(displayed).toBe(true)
-// })
+test('Button #duel shows up after select two (2) Add to Duo', async () => {
+    await clickDraw()
+    await clickAddToDuo()
+    await clickAddToDuo()
+    const divPlayerDuo = await driver.findElement(By.id('duel'))
+    const displayed = await divPlayerDuo.isDisplayed()
+    expect(displayed).toBe(true)
+})
