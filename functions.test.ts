07ab4a6b90f83a6c9e8f6d8fd2c8e958d35f3371
James Hager <james@avantiapplications.com>
2022-02-25 10:08:41 -0700

    Completed unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,15 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    const arr = [1,2,3,4,5]
+    const res = shuffleArray(arr)
+    console.log("res:", res)
+    console.log("typeof res:", typeof res)
+    console.log("isArray(res):", Array.isArray(res))
+    test('returns array', () => {
+        expect(Array.isArray(res)).toBeTruthy()
+    })
+    test('returns same-length array', () => {
+        expect(res.length).toBe(arr.length)
+    })
 })
