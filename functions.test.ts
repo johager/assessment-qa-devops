bae5cb1eb9f4b910e7b6bfc8e3bbc2ee10d9b9fd
James Hager <james@avantiapplications.com>
2022-02-25 12:22:50 -0700

    Add more unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -3,13 +3,26 @@ const {shuffleArray} = require('./utils')
 describe('shuffleArray should', () => {
     const arr = [1,2,3,4,5]
     const res = shuffleArray(arr)
-    console.log("res:", res)
-    console.log("typeof res:", typeof res)
-    console.log("isArray(res):", Array.isArray(res))
+
     test('returns array', () => {
+        console.log("isArray(res):", Array.isArray(res))
         expect(Array.isArray(res)).toBeTruthy()
     })
     test('returns same-length array', () => {
         expect(res.length).toBe(arr.length)
+    })
+    test('returns same items in array', () => {
+        const resSorted = res.slice().sort()
+        const sameElements = arr.every((val, index) => val === resSorted[index])
+        expect(sameElements).toBe(true)
+    })
+    test('returns shuffled array', () => {
+        let isSameVal = true
+        for (let i in arr) {
+            if (arr[i] != res[i]) {
+                isSameVal = false
+            }
+        }
+        expect(isSameVal).toBe(false)
     })
 })
